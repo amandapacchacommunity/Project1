e9b8d088203af5491420db513e8511398a988c55
amandapacchacommunity <pacchaamanda@gmail.com>
2026-03-09 11:34:15 -0400

    Update explore_data.py

--- analysis/explore_data.py
+++ analysis/explore_data.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
+# load museum dataset
 df = pd.read_csv("museum_visitors_nyc_chicago_5y_30000.csv")
 
+# preview dataset
 print(df.head())
 
-print(df.groupby("city")["revenue"].sum())
+# visitor totals by city
+city_visitors = df.groupby("city")["party_size"].sum()
+
+print("Visitors by City:")
+print(city_visitors)
+
+# revenue by city
+revenue = df.groupby("city")["revenue"].sum()
+
+print("Revenue by City:")
+print(revenue)
